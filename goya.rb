54b5f245106e9191e178d0c32b05495871bbc94b
ibitomohiro <68018348+ibitomohiro@users.noreply.github.com>
2020-08-04 19:40:11 +0900

    Update goya.rb

--- goya.rb
+++ goya.rb
@@ -1,5 +1,7 @@
 require 'cgi'
 cgi = CGI.new
+#フォームに'goya'という変数があるときに真
+if cgi.has_key?('goya') 
 # データを受け取った後、HTMLの形式でレスポンスを返す
 cgi.out("type" => "text/html", "charset" => "UTF-8") {
   # 情報の目印とした"goya"のデータを cgi['goya']と言う記述で取り出し、ローカル変数に代入する
@@ -11,23 +13,27 @@ cgi.out("type" => "text/html", "charset" => "UTF-8") {
       文字列：#{get}
     </body>
   </html>"
-
-  get_1 = cgi['goya1']
+}
+elsif cgi.has_key?('goya1')
+  cgi.out("type" => "text/html", "charset" => "UTF-8") {
+  get1 = cgi['goya1']
   # HTMLでレスポンスを返却する
   "<html>
     <body>
       <p>譲渡先が自家消費でないゴーヤの情報は下記になります</p>
-      文字列：#{get_1}
+      文字列：#{get1}
     </body>
   </html>"
-
-  get_2 = cgi['goya_false']
+  }
+elsif cgi.has_key?('goya_false')
+  cgi.out("type" => "text/html", "charset" => "UTF=-8") {
+  get2 = cgi['goya_false']
   # HTMLでレスポンスを返却する
   "<html>
     <body>
       <p>品質が悪いゴーヤの情報は下記になります</p>
-      文字列：#{get_2}
+      文字列：#{get2}
     </body>
   </html>"
-  
 }
+end
